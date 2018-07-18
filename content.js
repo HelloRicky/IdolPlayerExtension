aebb5b0892f551bae23efdf97d62713aea828fd3
Ricky <ricky.zheng2@gmail.com>
2018-07-18 16:58:51 +1000

    try to work out how to control youtube speed

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+//https://github.com/igrigorik/videospeed
+//https://github.com/zu1in/ytpb-speed-control
 
-console.log("hello" + firstHref);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      $('#ytp-menu-speed').parent().find('.ytp-button:contains("1.5")').click()
+
+      console.log(firstHref);
+    }
+  }
+);
